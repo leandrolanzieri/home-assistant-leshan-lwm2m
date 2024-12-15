ffb9d7c237f1fef6d2c4eadbb8b04d4113860d85
Leandro Lanzieri <leandro.lanzieri@haw-hamburg.de>
2024-12-15 14:48:53 +0000

    leshan_client/lwm2m_client: fix linting errors

--- custom_components/dev_leshan_lwm2m/leshan_client/lwm2m_client.py
+++ custom_components/dev_leshan_lwm2m/leshan_client/lwm2m_client.py
@@ -1,14 +1,14 @@
 """A LwM2M client at the Leshan server."""
 
 from dataclasses import dataclass
+
 from .objects import Lwm2mObjectInstance
 
 
 @dataclass
 class Lwm2mClient:
-    """
-    A LwM2M client at the Leshan server.
-    """
+    """A LwM2M client at the Leshan server."""
+
     endpoint: str
     "The endpoint name of the client."
 
@@ -42,16 +42,19 @@ class Lwm2mClient:
     object_instances: list[Lwm2mObjectInstance]
     "The object instances of the client."
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
+        """Initialize the object instances after the dataclass is created."""
         object_instances = []
-        for obj_id, instances in self.object_instances.items():
-            obj_id = int(obj_id)
-            for instance_id in instances:
-                object_instances.append(
-                    Lwm2mObjectInstance(obj_id, instance_id))
+
+        for obj_id, instances in self.object_instances.items():  # type: ignore[attr-defined]
+            obj_id_int = int(obj_id)
+            object_instances.extend(
+                Lwm2mObjectInstance(obj_id_int, instance_id)
+                for instance_id in instances
+            )
 
         self.object_instances = object_instances
 
     def __eq__(self, value: object) -> bool:
-        return isinstance(value, Lwm2mClient) and \
-            value.endpoint == self.endpoint
+        """Check if two Lwm2mClient instances are equal based on their endpoint."""
+        return isinstance(value, Lwm2mClient) and value.endpoint == self.endpoint
